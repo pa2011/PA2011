614ffea17b13eb9b343c392aa2829d4544a4d23e
Sandro Ropelato <sandro.ropelato@gmail.com>
2011-10-27 09:47:52 +0200

    Shader now supports Direct3D and OpenGL profiles.

--- C++/DrivingSimulator/Source/Test1/Scene1.cpp
+++ C++/DrivingSimulator/Source/Test1/Scene1.cpp
@@ -44,8 +44,8 @@ void Scene1::createScene()
 	sunLight->setSpecularColour(Ogre::ColourValue(0.3, 0.3, 0.3));
 
 	// enable shadow
-	//sceneManager->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
-	//sceneManager->setShadowFarDistance(500);
+	sceneManager->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
+	sceneManager->setShadowFarDistance(500);
 }
 
 bool Scene1::frameRenderingQueued(const Ogre::FrameEvent& evt)
