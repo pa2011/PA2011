3e0d78534d738fcf67630062e46828e9dadb294d
ETH1 <wurmlchr@students.zhaw.ch>
2011-12-21 18:28:45 +0100

    Changed number of mipmaps to 3 in City Scene. Deleted VideoPlayer (VLC version).

--- C++/VideoPlayer2/VideoPlayer2.bat
+++ C++/VideoPlayer2/VideoPlayer2.bat
@@ -4,5 +4,6 @@ set MPLAYER_PATH="D:\MPlayer\mplayer.exe"
 set VIDEO_PATH="D:\Videos\video2.mov"
 
 cd %0\..\
-..\Build\Windows\bin\Release\VideoPlayer2.exe %MPLAYER_PATH% %VIDEO_PATH% 5 150 100 52284 52285 "127.0.0.1"
+Build\Windows\bin\Release\VideoPlayer2.exe %MPLAYER_PATH% %VIDEO_PATH% 5 150 100 52284 52285 "127.0.0.1"
 
+pause
